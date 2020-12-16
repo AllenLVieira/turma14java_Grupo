3422300f861725c9b359469dfe7a2464462745c5
Danilo Mendes <danilmendes@gmail.com>
2020-12-16 10:53:40 -0300

    Add ContaCorrente (#1)

---
+++ src/bancos/ContaCorrente.java
@@ -0,0 +1,30 @@
+package bancos;
+
+import java.util.Scanner;
+
+public class ContaCorrente extends Conta 
+{
+	protected int numeroTalaoCheque;
+
+	public ContaCorrente(int numeroConta, String cpf) {
+		super(numeroConta, cpf);
+	}
+	public int getNumeroTalaoCheque() {
+		return numeroTalaoCheque;
+	}
+	
+	public void setNumeroTalaoCheque(int numeroTalaoCheque) {
+		this.numeroTalaoCheque = numeroTalaoCheque;
+	}
+	
+	public int retornaTalao(int quantidadeTalao) {		
+		if (quantidadeTalao < 0 || quantidadeTalao > 3) {
+			System.out.println("Quantidade inválida!");
+		}
+		else {
+			numeroTalaoCheque+=quantidadeTalao;
+		}
+		System.out.printf("Número de talões impressos até o momento: %d",numeroTalaoCheque);
+		return numeroTalaoCheque;
+	}
+}
